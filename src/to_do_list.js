f7598648e344fe5d62e78bdd8f7a227d8655bbb9
Avishek Kundu <119733078+avishekKundu@users.noreply.github.com>
2023-10-03 15:29:41 +0530

    Done

--- src/to_do_list.js
+++ src/to_do_list.js
@@ -21,34 +21,38 @@ const TODOLIST = () => {
 
     return (
         <>
-            <div className="container-fluid p-3 border border-dark border-2">
-                <div className="container-fluid p-3 border border-dark border-2">
+            <div className="main_content">
+                <div className="content_head">
                     <center>TO-DO-LIST</center>
                 </div>
-                <div className="container-fluid p-3 border border-dark border-2">
-                    <div className="task_input">
-                        <textarea
-                            placeholder="Enter task"
-                            rows={3} cols={50}
-                            value={task} onChange={(e) => setTask(e.target.value)} />
+                <div className="content_body">
+                    <div className="body_input">
+                        <div className="task_input">
+                            <textarea
+                                placeholder="Enter task"
+                                rows={2} cols={25}
+                                value={task} onChange={(e) => setTask(e.target.value)} />
+                        </div>
+                        <div className="add_btn">
+                            <button className="btn btn-primary rounded-1" onClick={addTask}>ADD</button>
+                        </div>
                     </div>
-                    <div className="add_btn">
-                        <button className="btn btn-primary" onClick={addTask}>ADD</button>
-                    </div>
-                </div>
-                <div className="container-fluid taskListContainer">
-                    <ul className="list-group task-list">
-                        {taskList.map((task, index) => (
-                            <li key={index} className="taskLists">
-                                <div className="card p-2 listContainer">
-                                    <div className="card-body cardBody">
-                                        <p className="card-text inputTask"><span>TASK:</span> {task}</p>
-                                        <button onClick={() => removeTask(index)} className="btn btn-danger border border-3 btnRemove">Delete</button>
+                    <div className="taskListContainer">
+                        <ul className="list-group" id="task_lists">
+                            {taskList.map((task, index) => (
+                                <li key={index} className="task_List">
+                                    <div className="card p-2">
+                                        <div className="card-body" id="cardBody">
+                                            <p className="card-text display_Task">TASK: <span>{task}</span></p>
+                                            <div className="btnRemove">
+                                                <button onClick={() => removeTask(index)} className="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </>
